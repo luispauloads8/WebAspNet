b1f8736e822633ab34c0e7859fb328b2bf4b57fd
luispauloads8 <luispaulo.ads8@gmail.com>
2021-08-30 22:15:12 -0300

    Seller details and eager loading

--- SalesWebMvc/Services/SellerService.cs
+++ SalesWebMvc/Services/SellerService.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
 
 namespace SalesWebMvc.Services
 {
@@ -17,7 +18,7 @@ namespace SalesWebMvc.Services
 
         public List<Seller> FindAll()
         {
-            return _context.Seller.ToList();
+            return _context.Seller.OrderBy(obj => obj.Name).ToList();
         }
 
         public void Insert(Seller obj)
@@ -28,7 +29,7 @@ namespace SalesWebMvc.Services
 
         public Seller FindById(int id)
         {
-            return _context.Seller.FirstOrDefault(obj => obj.Id == id); 
+            return _context.Seller.Include(obj => obj.Departament).FirstOrDefault(obj => obj.Id == id); 
         }
 
         public void Remove(int id)
