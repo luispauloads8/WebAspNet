3c5b3e36962ac0073d9c7711389f7225e28b6849
luispauloads8 <luispaulo.ads8@gmail.com>
2021-08-31 01:23:14 -0300

    Returning custom error page

--- SalesWebMvc/Models/ViewsModels/ErrorViewModel.cs
+++ SalesWebMvc/Models/ViewsModels/ErrorViewModel.cs
@@ -6,6 +6,7 @@ namespace SalesWebMvc.Models.ViewsModels
     {
         public string RequestId { get; set; }
 
+        public string Message { get; set; }
         public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
     }
 }
